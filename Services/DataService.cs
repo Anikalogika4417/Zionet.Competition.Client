ef04e20fccff4fe727bbc5767b41791e49e8fe58
Aniklogika4417 <anikalogika4417@gmail.com>
2023-01-05 01:53:03 +0200

    Updating task without file

--- Services/DataService.cs
+++ Services/DataService.cs
@@ -26,16 +26,28 @@ namespace Zionet.Competition.Client.Services
                 ($"https://localhost:44361/api/Task/MainTask");
         }
 
-        // public async Task<List<MBonusToTask>> GetBonusTaskById(RequestBonusToTask id_main_task)
-        // {
-        //     return await http.GetFromJsonAsync<List<MBonusToTask>>
-        //         ($"https://localhost:44361/api/BonusTask/ById", id_main_task);
-        // }
+        public async Task<List<MTask>> GetTaskById(int id_task)
+        {
+            return await http.GetFromJsonAsync<List<MTask>>
+                ($"https://localhost:44361/api/Task/ById?_id={id_task}");
+        }
+
+        
+        public async Task<List<MBonusToTask>> GetBonusTaskByMainId(int id_main_task)
+        {
+            return await http.GetFromJsonAsync<List<MBonusToTask>>
+                ($"https://localhost:44361/api/BonusTask/ByMainId?_id={id_main_task}");
+        }
 
         public async Task<List<MBonusToTask>> GetAllBonusTask()
         {
             return await http.GetFromJsonAsync<List<MBonusToTask>>
                 ($"https://localhost:44361/api/BonusTask");
+        }
+
+        public async Task UpdateTask(MTask task)
+        {
+            await http.PutAsJsonAsync<MTask>($"https://localhost:44361/api/TestTask", task);
         }
 
 
